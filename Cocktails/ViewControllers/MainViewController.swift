aafe7145d7ead7f25210f846853fdcb5ffb1ea0d
nurqalam <e-nurkalam.tolegen@homecredit.kz>
2023-06-25 19:16:07 +0500

    added packages

--- Cocktails/ViewControllers/MainViewController.swift
+++ Cocktails/ViewControllers/MainViewController.swift
@@ -9,9 +9,36 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    private let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+    
+    private lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        layout.minimumInteritemSpacing = 8
+        layout.minimumLineSpacing = 8
+        return collectionView
+    }()
+
+    private lazy var searchBar: UISearchBar = {
+        let bar = UISearchBar()
+        return bar
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        setup()
+    }
+    
+    private func setup() {
+        view.backgroundColor = .white
+        title = "Cocktails"
+        
+        setupViews()
+    }
+
+    private func setupViews() {
+        view.addSubview(searchBar)
+        view.addSubview(collectionView)
     }
 }
+
 
